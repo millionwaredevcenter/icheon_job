bea70e82a64b046a3c23da6da811763f6fc48500
JEONGHWAN,SEO <restnfeel@gmail.com>
2020-11-08 16:36:47 +0900

    작업사항 반영
    작업중

--- lib/screens/my_homepage.dart
+++ lib/screens/my_homepage.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
@@ -7,6 +8,9 @@ import 'package:icheon_job/models/emp_job.dart';
 import 'package:icheon_job/models/json_res.dart';
 import 'package:icheon_job/screens/job_list.dart';
 import 'package:icheon_job/screens/mw_intro.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
+
+import '../models/emp_job.dart';
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
@@ -22,13 +26,28 @@ class _MyHomePageState extends State<MyHomePage> {
   List<EmpJob> list = [];
 
   @override
-  void initState() {
-    // TODO: implement initState
+  void initState() async {
     super.initState();
-    getData();
+    List<EmpJob> inlist = await getData();
+    print(inlist);
+    setState(() {
+      list = inlist;
+    });
   }
 
-  void getData() async {
+  RefreshController _refreshController =
+      RefreshController(initialRefresh: false);
+
+  void onRefresh() async {
+    List<EmpJob> inlist = await getData();
+
+    setState(() {
+      list = inlist;
+    });
+    _refreshController.refreshCompleted();
+  }
+
+  Future<List<EmpJob>> getData() async {
     var url =
         "${DotEnv().env['DATAGG_API_URL']}/EmplmntInfoStus?KEY=${DotEnv().env['DATAGG_API_KEY']}&type=json&SIGUN_CD=41500";
     var response = await http.get(url);
@@ -36,15 +55,12 @@ class _MyHomePageState extends State<MyHomePage> {
     var wdata = JsonRes.fromJSON(json);
     var items = wdata.response[1]["row"];
 
-    List<EmpJob> inlist = [];
     for (var i = 0; i < items.length; i++) {
       final item = items[i];
-      inlist.add(EmpJob.formJson(item));
+      list.add(EmpJob.formJson(item));
     }
 
-    setState(() {
-      list = inlist;
-    });
+    return list;
     //print(inlist);
   }
 
@@ -71,15 +87,47 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              '아래로 당겨 새로고침',
-            ),
             Expanded(
-              child: ListView.builder(
-                  itemExtent: 100.0,
-                  itemCount: list.length,
-                  itemBuilder: (context, index) =>
-                      JobList(context, list[index], index)),
+              child: Text(
+                '아래로 당겨 새로고침',
+              ),
+            ),
+            SmartRefresher(
+              enablePullDown: true,
+              enablePullUp: true,
+              header: WaterDropHeader(),
+              footer: CustomFooter(
+                builder: (BuildContext context, LoadStatus mode) {
+                  Widget body;
+                  if (mode == LoadStatus.idle) {
+                    body = Text("pull up load");
+                  } else if (mode == LoadStatus.loading) {
+                    body = CupertinoActivityIndicator();
+                  } else if (mode == LoadStatus.failed) {
+                    body = Text("Load Failed!Click retry!");
+                  } else if (mode == LoadStatus.canLoading) {
+                    body = Text("release to load more");
+                  } else {
+                    body = Text("No more Data");
+                  }
+                  return Container(
+                    height: 55.0,
+                    child: Center(child: body),
+                  );
+                },
+              ),
+              controller: _refreshController,
+              onLoading: onRefresh,
+              onRefresh: onRefresh,
+              child: Container(
+                height: MediaQuery.of(context).size.height * 1.2,
+                width: MediaQuery.of(context).size.width * 3.4,
+                child: ListView.builder(
+                    itemExtent: 100.0,
+                    itemCount: list.length,
+                    itemBuilder: (context, index) =>
+                        JobList(context, list[index], index)),
+              ),
             ),
           ],
         ),
